2d8255e8e62d11f54ed6183fa58f19ec7ac51798
cauasturaro <caua.sturaro111@gmail.com>
2025-07-21 10:24:04 -0300

    adding students table & search function

--- frontend/src/app/pages/students/students.ts
+++ frontend/src/app/pages/students/students.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 interface Student {
   id: number;
@@ -11,25 +13,37 @@ interface Student {
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './students.html',
-  styleUrl: './students.css'
+  styleUrls: ['./students.css']
 })
 export class Students {
+
+  constructor(private router: Router) {}
+
+  searchStudent: string = '';
+
   students: Student[] = [
-    {
-      id: 1,
-      name: 'João',
-      age: 20,
-      email: 'joão@gmail.com',
-      course: 'Mathematics'
-    },
-    {
-      id: 2,
-      name: 'Maria',
-      age: 22,
-      email: 'maria@gmail.com',
-      course: 'ADS'
-    }
+    { id: 1, name: 'Cauã Sales Sturaro', age: 18, email: 'sturaro.contato@gmail.com', course: 'ADS' },
+    { id: 2, name: 'Maria Sobrenome Outro', age: 22, email: 'maria@gmail.com', course: 'Engenharia de Produção' }
   ];
+
+  filteredStudents(): Student[] {
+    if (!this.searchStudent) return this.students;
+
+    const search = this.searchStudent.toLowerCase();
+
+    return this.students.filter(student =>
+      String(student.id).includes(search) ||
+      student.name.toLowerCase().includes(search) ||
+      student.email.toLowerCase().includes(search) ||
+      student.course.toLowerCase().includes(search)
+    );
+  }
+
+  editStudent(id: number) {
+    // DESCOBRIR COMO FAZER FUNCIONAR:
+    // this.router.navigate(['/students/', id]);
+  }
+
 }
